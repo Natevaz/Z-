83bb3365a1da9d779adc749fb3e404675bb09ba2
Natevaz <54329202+Natevaz@users.noreply.github.com>
2019-08-29 09:05:13 -0500

    Says our major

---
+++ Hello/hello.c
@@ -0,0 +1,20 @@
+/**
+* Author Nate Vazquez and Brendon Goodrich
+* Date 2019/29/08
+* 
+* A simple hello world program in C
+*
+*
+*/
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+	printf("Nate Vazquez and Brendon Goodrich\n")
+
+	printf("Elecritcal Engineering and Computer Engineering")
+
+		return 0;
+
+}
